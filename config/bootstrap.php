4b77be5c611405cd72b620c0e961ccd07b6ae786
solilsan <alexsandrox1000x@gmail.com>
2019-03-04 23:28:00 +0100

    s

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,14 +7,13 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
-
-    die(var_dump("s"));
     $_SERVER += $env;
     $_ENV += $env;
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
     // load all the .env files
+    die(var_dump("s"));
     (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
 
